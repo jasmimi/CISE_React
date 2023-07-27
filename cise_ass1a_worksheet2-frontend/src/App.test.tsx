2819770ef3d91eb67434baca5622f024da492446
jasmimi <jasmine_amohia@hotmail.com>
2023-07-27 18:31:04 +1200

    test CICD with merge to main

--- cise_ass1a_worksheet2-frontend/src/App.test.tsx
+++ cise_ass1a_worksheet2-frontend/src/App.test.tsx
@@ -9,7 +9,7 @@ it('renders without crashing', () => {
 });
 
 describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
+  it('knows that 2 and 2 sum to 4', () => {
     expect(2 + 2).toBe(4);
   });
 })
